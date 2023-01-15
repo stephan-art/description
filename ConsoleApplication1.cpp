76c54fbe4bf4dc758ffa937b5eb5902d1c29c1f0
Stephan Michailenko <94607203+stephan-art@users.noreply.github.com>
2023-01-15 15:47:21 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string str;
+
+	getline(cin, str);
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] != ' ') {
+			str[i] += 2;
+			if (str[i] > 'z')str[i] -=26;
+		}
+	}
+
+	cout << str;
+	
+}
